b59e64e7bd44fddbadd5eab2bcd1f92d5d319144
Adam <am1macdonald@gmail.com>
2021-03-31 14:55:37 -0400

    -added function to draw the grid and then erase grid

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,38 @@
-let gameGrid = document.getElementById("game-grid");
+let gridSizeButton = document.getElementById("submit");
+let popup = document.getElementById("popup");
+
+
+let grid = document.getElementById("game-grid");
+
+gridSizeButton.addEventListener('click', () => {
+    let num = document.getElementById("grid-size-input").value;
+    makeGrid(num);
+    popup.style.display = "none";
+
+});
 
 function makeGrid(num) {
-    let gridItem = document.createElement('div');
-    gridItem.clasList.add('grid-item');
-    for(let i = 0; i < num; i++) {
-        document.getElementById("game-grid");
+    let gridItem = document.createElement("div");
+    gridItem.classList.add("grid-item");
+    grid.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
 
-    }
-}
+    for(let i = 0; i < (num * num); i++) {
+        let gridItemId = `gridItem${i}`;
+        let clone = gridItem.cloneNode();
+        clone.setAttribute("onmouseover", "this.style.backgroundColor='black'");
+        grid.appendChild(clone);
+    };
+};
+
+let reset = document.getElementById("reset");
+
+reset.addEventListener('click', () => {
+    removeChild(grid);
+    popup.style.display = "block";
+});
+
+function removeChild(grid){
+    while (grid.firstChild) {
+        grid.removeChild(grid.firstChild);
+    };
+};

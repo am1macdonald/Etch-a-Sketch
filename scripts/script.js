7363436418eb3c86829e0d0e285db0a76f613dbc
Adam <am1macdonald@gmail.com>
2021-03-30 20:28:48 -0400

    added popup box for input field

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,10 @@
-+
+let gameGrid = document.getElementById("game-grid");
+
+function makeGrid(num) {
+    let gridItem = document.createElement('div');
+    gridItem.clasList.add('grid-item');
+    for(let i = 0; i < num; i++) {
+        document.getElementById("game-grid");
+
+    }
+}
